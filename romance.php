50e94032bd8d01ba1df0620d2daeb750d4fb515b
bravoeight98 <39548925+bravoeight98@users.noreply.github.com>
2019-12-29 03:48:45 +0600

    added files

---
+++ romance.php
@@ -0,0 +1,27 @@
+	<?php
+		include_once 'includes/dbh.inc.php';
+	?>
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Romantic animes</title>
+</head>
+<body>
+<h3>All Romantic Animes</h3>
+</body>
+</html>
+<?php
+	$sql = "SELECT * FROM `animes` WHERE Genere_Name LIKE Romance" ;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["Anime_ID"]. " - Name: " . $row["Anime_Name"]. " " .$row["Anime_Cover_Picic"] "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
